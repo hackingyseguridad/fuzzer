c0061c3fefa5f04a9449436e6bb57a9394cc0ba6
hackingyseguridad <antonio_taboada@hotmail.es>
2019-02-08 21:21:24 +0100

    Update fuzzerhttps.sh

--- fuzzerhttps.sh
+++ fuzzerhttps.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 cat << "INFO"
-   __                          _     _   _
-  / _|                        | |   | | | |
- | |_ _   _ ___________ _ __  | |__ | |_| |_ _ __
- |  _| | | |_  /_  / _ \ '__| | '_ \| __| __| '_ \
- | | | |_| |/ / / /  __/ |    | | | | |_| |_| |_) |
- |_|  \__,_/___/___\___|_|    |_| |_|\__|\__| .__/  v1.0
-                                            | |
-         https://www.hackingyseguridad.com  |_|
-
+   __                          _     _   _             
+  / _|                        | |   | | | |            
+ | |_ _   _ ___________ _ __  | |__ | |_| |_ _ __  ___ 
+ |  _| | | |_  /_  / _ \ '__| | '_ \| __| __| '_ \/ __|
+ | | | |_| |/ / / /  __/ |    | | | | |_| |_| |_) \__ \
+ |_|  \__,_/___/___\___|_|    |_| |_|\__|\__| .__/|___/ v 2.0
+                                            | |        
+         http://www.hackingyseguridad.com   |_|        
 INFO
 if [ -z "$1" ]; then
         echo
+        echo "Genera CA root y certificados para las peticiones https
         echo "Descubre ficheros en  url de sitio web por 200 OK.. "
         echo "Uso: $0 <https://dominio.com>"
         echo "Tiempo estimado 1 hora .."
