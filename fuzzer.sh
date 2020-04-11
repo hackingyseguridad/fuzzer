b68bf3933384c21f167f3e5d72721daa750beb38
hackingyseguridad <antonio_taboada@hotmail.es>
2020-04-11 18:03:03 +0200

    Update fuzzer.sh

--- fuzzer.sh
+++ fuzzer.sh
@@ -25,7 +25,14 @@ for n in `cat diccionario.txt`
 
 do
         fqdn=$1"/"$n
-        if timeout 1 curl --cacert MyRootCA.crt -k -s $fqdn -I --silent|grep "100\|200\|300\|301\|302\|401\|403\|405\|500"
+        if timeout 1 curl --cacert MyRootCA.crt -k -s $fqdn -I --silent  \
+-H 'Pragma: no-cache' \
+-H 'Cache-Control: no-cache' \
+-H 'Upgrade-Insecure-Requests: 1' \
+-H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36' \
+-H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3' \
+-H 'Accept-Encoding: gzip, deflate, br' \
+-H 'Accept-Language: es-ES,es;q=0.9,en;q=0.8' \ |grep "100\|200\|300\|301\|302\|401\|403\|405\|500"
         then echo $fqdn
         fi
 done
