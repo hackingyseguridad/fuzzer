b1d48e329e7a22864361f6100e9ebf7433f317f4
hackingyseguridad <antonio_taboada@hotmail.es>
2021-12-28 08:12:08 +0100

    Update fuzzerauto.sh

--- fuzzerauto.sh
+++ fuzzerauto.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "fuzzer masivo a fqdn en fichero ip.txt"
 chmod 777 *
+echo "www.hackingyseguridad.com (2021)"
 echo "Para mantener como proceso ejecutar: nohup ./fuzzerauto.sh &"
 echo "Uso.: ./fuzzerauto.sh "
 for n in `cat ip.txt`
