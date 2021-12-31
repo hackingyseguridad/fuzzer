2654f7585a379555afc841508c63978762f64213
hackingyseguridad <antonio_taboada@hotmail.es>
2021-12-31 13:22:35 +0100

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -1,1 +1,1 @@
-wget https://raw.githubusercontent.com/hackingyseguridad/fuzzer/master/diccionario.txt -O "${path}/fuzzer" >/dev/null 2>&1
+wget https://raw.githubusercontent.com/hackingyseguridad/fuzzer/master/diccionario.txt 
