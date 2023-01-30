f7473e483dfc540811b6719d903968213ef8f0bc
hackingyseguridad <antonio_taboada@hotmail.es>
2023-01-30 21:49:16 +0100

    Update fuzzer3.sh

--- fuzzer3.sh
+++ fuzzer3.sh
@@ -1,5 +1,5 @@
 for i in `cat ip.txt`
 do
-ffuf -w diccionario.txt -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36" -u http://$i/FUZZ -mc 200,204,301,302,307,401
+ffuf -w diccionario.txt -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36" -u http://$i/FUZZ -mc 200,204,301,302,307,400,401,405
 echo $i
 done
