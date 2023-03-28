3c23357f301025a437fdef7fdff62c8d809e5e6d
hackingyseguridad <antonio_taboada@hotmail.es>
2023-03-28 15:29:48 +0200

    Update dirb.sh

--- dirb.sh
+++ dirb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "uso.: sh dirb.sh url" 
-dirb $1 -M 100,204,400,401,403,409,500,503 
-dirb $1 diccionario.txt -M 100,204,400,401,403,409,500,503 
-dirb $1 diccionario2.txt -M 100,204,400,401,403,409,500,503 
-dirb $1 diccionario3.txt -M 100,204,400,401,403,409,500,503 
+dirb $1 -M 100,204,307,400,401,403,409,500,503 
+dirb $1 diccionario.txt -M 100,204,307,400,401,403,409,500,503 
+dirb $1 diccionario2.txt -M 100,204,307,400,401,403,409,500,503 
+dirb $1 diccionario3.txt -M 100,204,307,400,401,403,409,500,503 
 
