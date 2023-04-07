438b74df4975004da2778fad2b6e2e29bfbdb172
hackingyseguridad <antonio_taboada@hotmail.es>
2023-04-07 20:36:15 +0200

    Update fuzz.sh

--- fuzz.sh
+++ fuzz.sh
@@ -8,7 +8,7 @@ cat << "INFO"
  |  _| | | |_  /_  /  | '_ \| __| __| '_ \/ __|
  | | | |_| |/ / / /   | | | | |_| |_| |_) \__ \
  |_|  \__,_/___/___\  |_| |_|\__|\__| .__/|___/ v 1.00
-           ALDEA DEL FRESNO / MADRID / ESPAÑA 
+           ALDEA DEL FRESNO / MADRID / ESPAÑA
            http://www.hackingyseguridad.com/
 INFO
 if [ -z "$1" ]; then
@@ -20,5 +20,6 @@ fi
 echo
 echo "Fuzz de: " $1
 echo
-dirb  $1 diccionario.txt -M 100,204,307,400,401,403,409,500,503 -f
+dirb  $1 diccionario.txt -M 100,204,307,400,401,403,409,500,503 -f -w  -z 99 -a "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0" -H "Accept: text/html, applicattion/xhtml+xml, application/xml;q=0.9,*/*;q=0.8"
+
 
