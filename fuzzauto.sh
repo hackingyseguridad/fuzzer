992cb6718d1ae465f33fa103ceaf148fdd2428e3
hackingyseguridad <antonio_taboada@hotmail.es>
2023-04-07 16:58:00 +0200

    Update fuzzauto.sh

--- fuzzauto.sh
+++ fuzzauto.sh
@@ -2,16 +2,10 @@
 echo "fuzzer masivo a fqdn en fichero ip.txt"
 chmod 777 *
 echo "www.hackingyseguridad.com (2023)"
-echo "Para mantener como proceso ejecutar: nohup ./fuzzerauto.sh &"
-echo "Uso.: ./fuzzerauto.sh "
+echo "Para mantener como proceso ejecutar: nohup ./fuzzauto.sh &"
+echo "Uso.: ./fuzzauto.sh "
 for n in `cat ip.txt`
-do echo "======>" $n
-for p in `cat diccionario.txt`
-        do
-fqdn="https://$n:443/$p"
-# echo "===>" $fqdn
-dirb  $fqdn diccionario.txt -M 100,204,307,400,401,403,409,500,503 -f
-        then echo $fqdn
-        fi
+        do fqdn="http://$n" ;  echo FQDN "===>" $fqdn ; dirb  $fqdn diccionario.txt -M 100,204,307,400,401,403,409,500,503 -w -f
 done
-done
+
+
