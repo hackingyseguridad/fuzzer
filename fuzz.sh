ddeb5540f74f1d3ec6b4cbfb39c1e37404e127cc
hackingyseguridad <antonio_taboada@hotmail.es>
2023-04-07 15:38:54 +0200

    Update fuzz.sh

--- fuzz.sh
+++ fuzz.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
-# (c) hacking y seguridad .com 2023
+# (c) Hacking y Seguridad .COM 2023
+#
 cat << "INFO"
-   __                  _     _   _             
-  / _|                | |   | | | |            
- | |_ _   _ _________ | |__ | |_| |_ _ __  ___ 
+   __                  _     _   _
+  / _|                | |   | | | |
+ | |_ _   _ _________ | |__ | |_| |_ _ __  ___
  |  _| | | |_  /_  /  | '_ \| __| __| '_ \/ __|
  | | | |_| |/ / / /   | | | | |_| |_| |_) \__ \
  |_|  \__,_/___/___\  |_| |_|\__|\__| .__/|___/ v 1.00
-                                            | |        
-         http://www.hackingyseguridad.com   |_|        
+
+           http://www.hackingyseguridad.com/
 INFO
 if [ -z "$1" ]; then
         echo
         echo "Uso: $0 <https://dominio.com>"
-        echo "Tiempo estimado 1 hora ..."
+        echo "Tiempo estimado 2 hora ..."
         exit 0
 fi
 echo
-echo "Fuzzer de: " $1
+echo "Fuzz de: " $1
 echo
-dirb $1 diccionario.txt -M 100,204,307,400,401,403,409,500,503
+dirb  $1 diccionario.txt -M 100,204,307,400,401,403,409,500,503 -f
+
