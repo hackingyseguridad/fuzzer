ba4bfa7664e378d7da60095af0817509db64a7fd
hackingyseguridad <antonio_taboada@hotmail.es>
2023-10-26 12:17:07 +0200

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
-echo 
-echo "... actualizando diccionario ...  (R) 2023 hackingyseguridad.com "
-wget --inet4-only  https://raw.githubusercontent.com/hackingyseguridad/fuzzer/master/diccionario.txt -q -O diccionario.txt
 echo
-ecbo "actualizado!
+echo "... actualizando diccionario ...  (R) 2023 hackingyseguridad.com "
+wget https://raw.githubusercontent.com/hackingyseguridad/fuzzer/master/diccionario.txt -q -O diccionario.txt  --inet4-only
+echo
+echo "."
+echo ".."
+echo "..."
+echo
+echo "actualizado !!! "
+
+
