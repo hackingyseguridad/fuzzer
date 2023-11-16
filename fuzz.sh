ebd538183617487e1ad193d084dfca32119cf6a8
hackingyseguridad <antonio_taboada@hotmail.es>
2023-11-16 15:58:11 +0100

    Update fuzz.sh

--- fuzz.sh
+++ fuzz.sh
@@ -20,4 +20,17 @@ fi
 echo
 echo "Fuzz de: " $1
 echo
+echo "Cod Significado"
+echo "--- -----------"
+echo "200 OK" 
+echo "301 Movido permamentemente"
+echo "302 Encontrado"
+echo "304 No modificado"
+echo "400 Solicitud incorrecta"
+echo "400 No autorizado"
+echo "403 Prohibido"
+echo "404 No encontrado"
+echo "410 Ya no esta disponible"
+echo "500 Error interno en el servidor"
+echo
 dirb  $1 diccionario.txt -N 302 204 307 400 401 403 409 500 503 -b -f -w -S -z 99 -a "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0" -H "Accept: text/html, applicattion/xhtml+xml, application/xml;q=0.9,*/*;q=0.8"
