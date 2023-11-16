85d771139fe86c7cc9062cda5ca9f4551d116a77
hackingyseguridad <antonio_taboada@hotmail.es>
2023-11-16 20:14:02 +0100

    Update fuzzer2.sh

--- fuzzer2.sh
+++ fuzzer2.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-# (c) hacking y seguridad .com 2022
+# (c) hacking y seguridad .com 2024
 cat << "INFO"
-   __                          _     _   _
-  / _|                        | |   | | | |
- | |_ _   _ ___________ _ __  | |__ | |_| |_ _ __  ___
+   __                          _     _   _             
+  / _|                        | |   | | | |            
+ | |_ _   _ ___________ _ __  | |__ | |_| |_ _ __  ___ 
  |  _| | | |_  /_  / _ \ '__| | '_ \| __| __| '_ \/ __|
  | | | |_| |/ / / /  __/ |    | | | | |_| |_| |_) \__ \
- |_|  \__,_/___/___\___|_|    |_| |_|\__|\__| .__/|___/ v 3.01
-                                            | |
-         http://www.hackingyseguridad.com   |_|
+ |_|  \__,_/___/___\___|_|    |_| |_|\__|\__| .__/|___/ v 3.00
+                                            | |        
+         http://www.hackingyseguridad.com   |_|        
 INFO
 if [ -z "$1" ]; then
         echo
@@ -20,6 +20,19 @@ if [ -z "$1" ]; then
 fi
 echo
 echo "Fuzzer de: " $1
+echo
+echo "Cod Significado"
+echo "--- -----------"
+echo "200 OK" 
+echo "301 Movido permamentemente"
+echo "302 Encontrado "
+echo "304 No modificado"
+echo "400 Solicitud incorrecta"
+echo "400 No autorizado"
+echo "403 Prohibido"
+echo "404 No encontrado"
+echo "410 Ya no esta disponible"
+echo "500 Error interno en el servidor"
 echo
 
 for n in `cat diccionario.txt`
@@ -34,6 +47,6 @@ do
 -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3' \
 -H 'Accept-Encoding: gzip, deflate, br' \
 -H 'Accept-Language: es-ES,es;q=0.9,en;q=0.8' \ |grep "HTTP/2 100\|HTTP/2 200\|HTTP/2 300\|301\|HTTP/2 302\|HTTP/2 401\|HTTP/2 403\|HTTP/2 405\|HTTP/2 500"
-        then echo $fqdn
+        then echo $fqdn && echo
         fi
 done
