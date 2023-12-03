446ef4f3257fc6038ec8cb1fc128d29c90a4484e
hackingyseguridad <antonio_taboada@hotmail.es>
2023-12-03 17:56:05 +0100

    Update nmap.sh

--- nmap.sh
+++ nmap.sh
@@ -4,4 +4,4 @@ echo "(C) hackingyseguridad.com 2023"
 echo
 echo "Uso.: sh nmap.sh IP/fqdn"
 echo
-nmap $1 $2 -Pn -sV --open --script=http-enum --script-args http-enum.basepath=diccionario.txt  
+nmap $1 $2 $3 $4 $5 -Pn -sV --open --script=http-enum --script-args http-enum.basepath=diccionario.txt  
