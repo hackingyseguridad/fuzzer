d89ed245a1f4ccb5c46973745e4537563a5cab12
hackingyseguridad <antonio_taboada@hotmail.es>
2023-12-07 22:08:10 +0100

    Update fuzzer4.sh

--- fuzzer4.sh
+++ fuzzer4.sh
@@ -1,4 +1,4 @@
 echo
 echo "Usdo: sh fuzzer.sh http://www.hackingyseguridad.com"
 # diccionario:  /usr/share/wordlists/dirb/common.txt
-gobuster dir -e -u $1 -w diccionario.txt --no-error -z -k -a "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0" -H "Accept: text/html, applicattion/xhtml+xml, application/xml;q=0.9,*/*;q=0.8"
+gobuster dir -e -u https://www.zener.es -w diccionario.txt --no-error -z -k -a "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0"
