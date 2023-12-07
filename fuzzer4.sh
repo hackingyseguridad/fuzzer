1f6542174cdd8b7f145f427bbe77e31f9cc2a977
hackingyseguridad <antonio_taboada@hotmail.es>
2023-12-07 22:02:00 +0100

    Update fuzzer4.sh

--- fuzzer4.sh
+++ fuzzer4.sh
@@ -1,4 +1,4 @@
 echo
 echo "Usdo: sh fuzzer.sh http://www.hackingyseguridad.com"
 # diccionario:  /usr/share/wordlists/dirb/common.txt
-gobuster dir -e -u $1 -w diccionario.txt --no-error -z 
+gobuster dir -e -u $1 -w diccionario.txt --no-error -z -k -a "
