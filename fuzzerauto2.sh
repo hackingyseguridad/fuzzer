c63568e4dd459b567e18ef8016731cd8e1f5adec
hackingyseguridad <antonio_taboada@hotmail.es>
2024-01-29 22:13:08 +0100

    Create fuzzerauto2.sh

---
+++ fuzzerauto2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "fuzzer masivo a fqdn en fichero ip.txt"
+chmod 777 *
+echo "www.hackingyseguridad.com (2024)"
+echo "Para mantener como proceso ejecutar: nohup ./fuzzerauto.sh &"
+echo "Uso.: ./fuzzerauto2.sh "
+for n in `cat ip.txt`
+do echo "======>" $n
+        fqdn="https://$n:443"
+        echo "===>" $fqdn
+        gobuster dir -e -u https://$n -w diccionario.txt --no-error -z -k -a "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0" -r $1 $2
+done
