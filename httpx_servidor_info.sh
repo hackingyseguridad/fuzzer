be08dd336234f596daa0264cf96493c43b683923
hackingyseguridad <antonio_taboada@hotmail.es>
2024-02-27 14:39:04 +0100

    Update httpx_servidor_info.sh

--- httpx_servidor_info.sh
+++ httpx_servidor_info.sh
@@ -1,4 +1,4 @@
 echo
 echo
 echo
-cat http.txt | httpx -title -status-code -tech-detect -follow-redirects -ports 80,443,6443,7443,8000,8080,8888,9443,10443,8089,4433,8443,55443
+cat http.txt | httpx -title -status-code -tech-detect -follow-redirects cat http.txt | httpx -title -status-code -tech-detect -follow-redirects -ldp
