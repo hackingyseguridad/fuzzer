bc3728266d98e8629ed6ff48acbd09c32022c4f4
hackingyseguridad <antonio_taboada@hotmail.es>
2024-03-02 18:58:11 +0100

    Update httpx_code.sh

--- httpx_code.sh
+++ httpx_code.sh
@@ -1,4 +1,6 @@
 echo
 echo "codigo de respuesta http" 
 echo
-cat http.txt | httpx -ports 80,443,7443,8000,8009,8080,8081,8090,8180,8443,9443,10443 -path / -status-code -content-length 
+"cat http.txt | httpx -ports 80,443,7443,8000,8009,8080,8081,8090,8180,8443,9443,10443 -path / -status-code -content-length"
+cat http.txt | httpx -path / -status-code -content-length -leave-default-ports
+
