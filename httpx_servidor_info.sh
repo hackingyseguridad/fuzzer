59ce75fb9d5d4d912c6a6a291db8e787cb6335f4
hackingyseguridad <antonio_taboada@hotmail.es>
2024-03-12 08:42:21 +0100

    Update httpx_servidor_info.sh

--- httpx_servidor_info.sh
+++ httpx_servidor_info.sh
@@ -1,4 +1,5 @@
 echo
 echo
 echo
-cat http.txt | httpx -title -status-code -tech-detect -follow-redirects cat http.txt | httpx -title -status-code -tech-detect -follow-redirects -ldp
+echo "cat http.txt | httpx -title -status-code -tech-detect -follow-redirects" 
+cat http.txt | httpx -title -status-code -tech-detect -follow-redirects -ldp
