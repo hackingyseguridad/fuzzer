c437918f7fdea4b547e0b60e40072d1550284958
hackingyseguridad <antonio_taboada@hotmail.es>
2025-10-06 15:48:48 +0200

    Update fuzzerauto2.sh

--- fuzzerauto2.sh
+++ fuzzerauto2.sh
@@ -9,5 +9,5 @@ do echo "======>" $n
 
         fqdn="https://$n:443"
         echo "===>" $fqdn
-        gobuster dir -e -u $fqdn -w diccionario.txt --no-error -z -k -a "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0" -r $1 $2
+        gobuster dir -e -u $fqdn -w diccionario.txt --no-error -k -a "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0" -r $1 $2
 done
