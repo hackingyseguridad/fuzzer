ad18c244b244a0c304725dfe594868e84b2a3248
hackingyseguridad <antonio_taboada@hotmail.es>
2025-11-02 16:48:18 +0100

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
-echo
+echo "##################################################################"
 echo "... actualizando diccionarios ...  (R) 2025 hackingyseguridad.com "
+echo "##################################################################"
 echo
-wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/refs/heads/master/ficheros.txt -q -O diccionario.txt  --inet4-only
-wc -l diccionario.txt
+wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/refs/heads/master/ficheros.txt -q -O ficheros.txt  --inet4-only
+wc -l ficheros.txt
 echo ".."
 echo "..."
-wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/refs/heads/master/ficheros2.txt -q -O diccionario2.txt  --inet4-only
-wc -l diccionario2.txt
+wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/refs/heads/master/ficheros2.txt -q -O ficheros2.txt  --inet4-only
+wc -l ficheros2.txt
+echo "...."
+echo "....."
+wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/refs/heads/master/carpetas.txt -q -O carpetas.txt  --inet4-only
+wc -l carpetas.txt
 echo "...."
 echo "....."
 echo
