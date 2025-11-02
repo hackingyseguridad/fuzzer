0632dcc85ab02539022e9e7927f436b1f992825f
hackingyseguridad <antonio_taboada@hotmail.es>
2025-11-02 17:17:42 +0100

    Update fuzzerauto.sh

--- fuzzerauto.sh
+++ fuzzerauto.sh
@@ -6,7 +6,7 @@ echo "Para mantener como proceso ejecutar: nohup ./fuzzerauto.sh &"
 echo "Uso.: ./fuzzerauto.sh "
 for n in `cat ip.txt`
 do echo "======>" $n
-for p in `cat diccionario.txt`
+for p in `cat ficheros.txt`
 do
         fqdn="https://$n:443/$p"
 #       	echo "===>" $fqdn
