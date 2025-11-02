034a3d92248df2cc576abecb4481ec4ef0e602b2
hackingyseguridad <antonio_taboada@hotmail.es>
2025-11-02 17:09:04 +0100

    Update fuzzauto.sh

--- fuzzauto.sh
+++ fuzzauto.sh
@@ -5,7 +5,7 @@ echo "www.hackingyseguridad.com (2023)"
 echo "Para mantener como proceso ejecutar: nohup ./fuzzauto.sh &"
 echo "Uso.: ./fuzzauto.sh "
 for n in `cat ip.txt`
-        do fqdn="http://$n" ;  echo FQDN "===>" $fqdn ; dirb  $fqdn diccionario.txt -M 100,204,307,400,401,403,409,500,503 -w -f
+        do fqdn="http://$n" ;  echo FQDN "===>" $fqdn ; dirb  $fqdn archivos.txt -M 100,204,307,400,401,403,409,500,503 -w -f
 done
 
 
