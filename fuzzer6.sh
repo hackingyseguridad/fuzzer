e1688f3474a1da0a535e1829aa254ea00997641c
hackingyseguridad <antonio_taboada@hotmail.es>
2026-01-10 20:23:34 +0100

    Update fuzzer6.sh

--- fuzzer6.sh
+++ fuzzer6.sh
@@ -1,3 +1,8 @@
+# Fuzzer web
+# @antonio_taboada
+# hackingyseguridad.com 2026
 
 
 ffuf -w ficheros.txt -u $1/FUZZ $2 -mc 200 -r
+ffuf -w ficheros.txt -u $1/FUZZ $2 -mc 200 -r -X POST
+ffuf -w ficheros.txt -u $1/FUZZ $2 -mc 200 -r -X PUT
