35e07e8ee3e1cedcfda3286166b94ca649c39f3f
hackingyseguridad <antonio_taboada@hotmail.es>
2026-01-11 17:00:48 +0100

    Update fuzzer5.sh

--- fuzzer5.sh
+++ fuzzer5.sh
@@ -1,1 +1,8 @@
+
+echo
+echo "Uso: sh fuzzer5.sh http://www.hackingyseguridad.com"
+
+# diccionario Kali :  /usr/share/wordlists/dirb/common.txt
+
+gobuster dir -e -u $1 $2 -w ficheros.txt --no-error -z -k -a "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0" -r
 gobuster dir -e -u $1 $2 -w ficheros.txt -x .php,.html
