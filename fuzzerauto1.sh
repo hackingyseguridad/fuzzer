3e8658e5b5bf2ae3c9d2d96cfdba1dce74d32dce
hackingyseguridad <antonio_taboada@hotmail.es>
2026-01-11 18:25:01 +0100

    Create fuzzerauto1.sh

---
+++ fuzzerauto1.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+while read host
+do
+    url="https://$host"
+    echo "Probando ===================================>    $url"
+
+    wfuzz -w ficheros.txt \
+          -u "$url/FUZZ" \
+          $1 \
+          --hc 301,302,400,401,403,404,405,411,500,503  \
+          -X GET
+done < ip.txt
