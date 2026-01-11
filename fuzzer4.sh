971ff1081e47e5ebb7354446a093d5c3347c7dfa
hackingyseguridad <antonio_taboada@hotmail.es>
2026-01-11 17:02:09 +0100

    Update usage instructions and change fuzzer command

--- fuzzer4.sh
+++ fuzzer4.sh
@@ -1,4 +1,7 @@
 echo
-echo "Usdo: sh fuzzer.sh http://www.hackingyseguridad.com"
-# diccionario:  /usr/share/wordlists/dirb/common.txt
-gobuster dir -e -u $1 $2 -w ficheros.txt --no-error -z -k -a "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0" -r
+echo "Uso: sh fuzzer4.sh http://www.hackingyseguridad.com"
+
+# diccionario Kali:  /usr/share/wordlists/dirb/common.txt
+
+wfuzz -c -z file,ficheros.txt --hc 404 $1/FUZZ
+
