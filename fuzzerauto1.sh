0eb7736d3e871bb63ab187b9a22112b7341a54f1
hackingyseguridad <antonio_taboada@hotmail.es>
2026-01-12 09:24:41 +0100

    Update fuzzerauto1.sh

--- fuzzerauto1.sh
+++ fuzzerauto1.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+TIMEOUT=15
+
 while read host
 do
     url="https://$host"
     echo "Probando ===================================>    $url"
 
-    wfuzz -w ficheros.txt \
-          -u "$url/FUZZ" \
-          $1 \
-          --hc 301,302,400,401,403,404,405,411,500,503  \
-          -X GET
+        timeout $TIMEOUT wfuzz -w ficheros.txt \
+              -u "$url/FUZZ" \
+              $1 \
+              --hc 301,302,400,401,403,404,405,411,500,503 \
+              -X GET
+    echo "..."
 done < ip.txt
+
