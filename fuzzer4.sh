7f8510d159755923a1f99d8c2a92db53dffc6fbc
hackingyseguridad <antonio_taboada@hotmail.es>
2026-01-12 12:29:03 +0100

    Update fuzzer4.sh

--- fuzzer4.sh
+++ fuzzer4.sh
@@ -3,5 +3,5 @@ echo "Uso: sh fuzzer4.sh http://www.hackingyseguridad.com"
 
 # diccionario Kali:  /usr/share/wordlists/dirb/common.txt
 
-wfuzz -c -z file,ficheros.txt --hc 400,403,404 $1/FUZZ
+wfuzz -c -z file,ficheros.txt --hc 301,302,400,401,403,404,405,411,500,503 $1/FUZZ
 
