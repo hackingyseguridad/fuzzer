bfd7013f107a94d69538c38ed40902d3c1277ee8
hackingyseguridad <antonio_taboada@hotmail.es>
2026-02-03 22:32:33 +0100

    Update fuzzernmap.sh

--- fuzzernmap.sh
+++ fuzzernmap.sh
@@ -2,4 +2,4 @@
 # (r) hackingyseguridad.com 2026
 # Fuzzer con nmap
 # unzip http-fingerprints.lua.zip
-nmap -Pn -p 443 $1 $2 -sV --script=http-enum --script-args=http-enum.fingerprintfile=http-fingerprints.lua --open
+nmap -Pn -p 443 $1 $2 -sV --script=http-enum --script-args=http-enum.fingerprintfile=http-fingerprints.lua --open -oN resultado.txt 
